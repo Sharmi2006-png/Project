8a6fa7fdaab60f67de40642e5494f265c126d75d
Sharmi M <sharmimanikandan2@gmail.com>
2026-05-19 15:32:38 +0530

    Add files via upload

---
+++ Surprise/script.js
@@ -0,0 +1,19 @@
+function showSurprise(){
+
+    document
+    .getElementById("gallery")
+    .classList
+    .remove("hidden");
+
+    document
+    .getElementById("videosSection")
+    .classList
+    .remove("hidden");
+
+    document
+    .getElementById("finalMessage")
+    .classList
+    .remove("hidden");
+
+    
+}
